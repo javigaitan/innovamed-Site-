f8120d778e32ee380e478d67db85fb9f003fe955
Guadalupe Palmero <guadalupe.palmeroui@gmail.com>
2024-01-30 19:53:11 -0300

    flecha corregida

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -125,9 +125,9 @@ function Navbar() {
                                             >
                                                 Galeria
                                                 <div>
-                                                    <svg className="fill-stroke text-white dark:text-white" width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    {/* <svg className="fill-stroke text-white dark:text-white" width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M4.5 3L7.5 6L4.5 9" stroke="currentColor" strokeWidth="0.75" strokeLinecap="round" strokeLinejoin="round" />
-                                                    </svg>
+                                                    </svg> */}
                                                 </div>
                                             </button>
                                         </li>
